2caa640b676a80a51e36937cd468f3b685a05c97
TSanni <tomas.sanni@gmail.com>
2022-08-29 09:11:22 -0500

    Initial Commit

---
+++ GWeather/GWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GWeatherApp.swift
+//  GWeather
+//
+//  Created by Tomas Sanni on 8/29/22.
+//
+
+import SwiftUI
+
+@main
+struct GWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
